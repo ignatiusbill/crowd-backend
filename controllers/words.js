b070ee47e9b4efea7a0107ed4001c63dfa7f1028
ignatiusbill <ignatiusbill@gmail.com>
2017-08-29 12:54:20 -0700

    Removed populate.js (a test for communicating with MongoDB).

--- controllers/words.js
+++ controllers/words.js
@@ -1,7 +1,5 @@
 import Word from '../models/word';
 
 export const index = (req, res, next) => {
-    Word.find().lean().exec((err, words) => res.json(
-        { words }
-    ));
+    Word.find().lean().exec((err, words) => res.json({ words }));
 };
