4ac94d949190ba82d1b4f2bf16d44e8f983e5817
ignatiusbill <ignatiusbill@gmail.com>
2017-08-29 13:11:33 -0700

    Added path import.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-import express, { static } from 'express';
+import express from 'express';
+import path from 'path';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import router from './router';
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(morgan('combined'));
 app.use('/v1', router);
-app.use('/static', static(path.join(__dirname, 'assets/sounds/')))
+app.use('/static', express.static(path.join(__dirname, '/assets/sounds')))
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), () => {
